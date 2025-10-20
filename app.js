15f3114ee101d2039cb831b31396557e01e84ba0
Hamza Ahmed Shaikh <99156466+hamzaahmedshaikh@users.noreply.github.com>
2025-10-21 01:51:02 +0500

    Remove seeMore function and add background color changecommit

--- app.js
+++ app.js
@@ -1,28 +1,8 @@
-var input = document.getElementById("input");
+let color = ["red" , "yellow" , "green" , "pink"]
 
-var btn = document.getElementsByTagName("button")[0];
-var p = document.getElementsByTagName("p")[0];
+function toChangeBackground(){
+  let num = Math.floor(Math.random() * color.length)
+  document.body.style.backgroundColor = color[num]
 
-var shortPara = "I'm a front-end web developer";
-var longPara =
-  "with over 4 years of experience creating visually stunning and responsive websites and web applications using frameworks like React and Bootstrap";
-
-function seeMore() {
-  if (btn.innerText == "seeMore") {
-    p.innerText = longPara;
-    btn.innerText = "See Less";
-  } else {
-    p.innerText = shortPara;
-    btn.innerText = "seeMore";
-  }
+  document.getElementsByTagName("h1")[0].innerText =color[num]
 }
-
-// function seeMore() {
-//     if (btn.innerText == "SeeMore") {
-//         p.innerText = longPara
-//         btn.innerText = "See Less"
-//     } else {
-//         p.innerText = shortPara
-//         btn.innerText = "SeeMore"
-//     }
-// }
