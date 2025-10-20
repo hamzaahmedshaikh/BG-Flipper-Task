b15ea83b302315f2ed21d6b13f36485a91b5fd5c
Hamza Ahmed Shaikh <99156466+hamzaahmedshaikh@users.noreply.github.com>
2025-10-21 01:51:39 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
+// var input = document.getElementById("input");
+
+// var btn = document.getElementsByTagName("button")[0];
+// var p = document.getElementsByTagName("p")[0];
+
+// var shortPara = "I'm a front-end web developer";
+// var longPara =
+//   "with over 4 years of experience creating visually stunning and responsive websites and web applications using frameworks like React and Bootstrap";
+
+// function seeMore() {
+//   if (btn.innerText == "seeMore") {
+//     p.innerText = longPara;
+//     btn.innerText = "See Less";
+//   } else {
+//     p.innerText = shortPara;
+//     btn.innerText = "seeMore";
+//   }
+// }
+
+// function seeMore() {
+//     if (btn.innerText == "SeeMore") {
+//         p.innerText = longPara
+//         btn.innerText = "See Less"
+//     } else {
+//         p.innerText = shortPara
+//         btn.innerText = "SeeMore"
+//     }
+// }
+
 let color = ["red" , "yellow" , "green" , "pink"]
 
 function toChangeBackground(){
